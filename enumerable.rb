f4667f01e566ce2d051c9f57137e509af4812555
ForHemer <foreste.hemerson@gmail.com>
2022-05-06 12:45:31 -0400

    Update MyEnumerable module

--- enumerable.rb
+++ enumerable.rb
@@ -1,9 +1,19 @@
 module MyEnumerable
   def all?
     result = true
-
     each { |n| result = false unless yield n }
-
     result
+  end
+
+  def any?
+    result = false
+    each { |n| bool = true if yield n }
+    result
+  end
+
+  def filter
+    arr = []
+    each { |n| arr.push(n) if yield n }
+    arr
   end
 end
